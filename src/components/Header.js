3cc3a21c875947c1ba6a66d8220123f61a73273f
Nischal Khadka <justchalchal@gmail.com>
2022-08-23 08:55:06 +0545

    final commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <nav>
+    <div>
+        <img src ="./troll.png" />
+        <h1>MemeGenerator</h1>
+    </div>
+    <h3>Apple And Oranges</h3>
+    </nav>
+  )
+}
+
+export default Header
